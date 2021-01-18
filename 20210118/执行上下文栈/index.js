6613b42009488b0e0fef74c306f4ed6326c740ba
tian.zhang <382785079@qq.com>
2021-01-18 18:10:57 +0800

    20210118[打卡] 1、执行上下文

---
+++ 20210118/执行上下文栈/index.js
@@ -0,0 +1,17 @@
+/*
+ * 执行上下文栈
+ */
+
+// function fun3() {
+//     console.log('fun3')
+// }
+//
+// function fun2() {
+//     fun3();
+// }
+//
+// function fun1() {
+//     fun2();
+// }
+//
+// fun1();
