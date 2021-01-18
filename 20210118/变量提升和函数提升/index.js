abd56fbdd53c8cb8ef71e069a9e751369c1f37cd
tian.zhang <382785079@qq.com>
2021-01-18 22:03:12 +0800

    20210118[打卡] 1、变量提升新增面试题（关于变量提升和函数提升的嵌套关系）

--- 20210118/变量提升和函数提升/index.js
+++ 20210118/变量提升和函数提升/index.js
@@ -45,3 +45,21 @@
 //     console.log('named function expression');
 // };
 // foo() // named function expression
+
+
+// var fn = null;
+// function foo() {
+//     var a = 2;
+//     function innnerFoo() {
+//         console.log(c);
+//         console.log(a);
+//     }
+//     fn = innnerFoo;
+// }
+// function bar() {
+//     var c = 100;
+//     fn();
+// }
+//
+// foo();
+// bar();
