7dcd1590c46c4f61b6e96684314a35e0b15277a7
tian.zhang <382785079@qq.com>
2021-01-21 20:32:18 +0800

    20210121[打卡] 1、前端跨域 2、常见事件

--- ZTTUO/JavaScript/ES6/index.js
+++ ZTTUO/JavaScript/ES6/index.js
@@ -62,3 +62,24 @@ console.log(
 
 
 
+function Foo() {
+    var i = 0;
+    return function() {
+        console.log(i++);
+    }
+}
+
+var f1 = Foo(),
+    f2 = Foo();
+f1();
+f1();
+f2();
+
+
+var bb = 1;
+function aa(bb) {
+    bb = 2;
+    alert(bb);
+};
+aa(bb);
+alert(bb);
