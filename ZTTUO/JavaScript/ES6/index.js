e4bc716a2319a514dba994f94b7b00532cea5744
tian.zhang <382785079@qq.com>
2021-01-21 18:08:20 +0800

    20210121[打卡] 1、关于函数式编程带来的思考和学习

---
+++ ZTTUO/JavaScript/ES6/index.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+        key: 'name',
+        value: '字节跳动'
+    },
+    {
+        key: 'age',
+        value: 1
+    },
+    {
+        key: 'from',
+        value: '数据平台'
+    }
+];
+
+const processFN = data => data.reduce((obj, {key, value}) => ({...obj, [key]: value}), {});
+
+console.log(processFN(data))
+
+for (var item of data) {
+    for (var key in item) {
+        console.log('键：' + key)
+        console.log('值：' + item[key])
+    }
+}
+
+const dataItem = {
+    key: 'from',
+    value: '数据平台'
+}
+
+console.log('key：' + Object.getOwnPropertyNames(dataItem)) //[key,value]
+
+
+class Functor {
+    constructor(val) {
+        this.val = val;
+    }
+
+    map(f) {
+        return new Functor(f(this.val));
+    }
+}
+
+console.log(
+    (new Functor('flamethrowers')).map(function(s) {
+        return s.toUpperCase();
+    })
+)
+
+Functor.of = function(val) {
+    return new Functor(val);
+};
+
+console.log(
+    Functor.of(2).map(function (two) {
+        return two + 2;
+    })
+    // Functor(4)
+)
+
+
+
+
