7d0c031648d28330db5cc73508b670a692978478
tian.zhang <382785079@qq.com>
2021-01-26 23:40:58 +0800

    20210126[打卡]

--- ZTTUO/JavaScript/ES6/index.js
+++ ZTTUO/JavaScript/ES6/index.js
@@ -167,3 +167,37 @@
 // x[2] = z = 30;
 // console.log(x, y, z); // [10, 2, 30], [4, 20, 6], 30
 
+// function Otaku (name, age) {
+//     this.name = name;
+//     this.age = age;
+//
+//     this.habit = 'Games';
+// }
+//
+// Otaku.prototype.strength = 60;
+//
+// Otaku.prototype.sayYourName = function () {
+//     console.log('I am ' + this.name);
+// }
+//
+// function objectFactory() {
+//
+//     var obj = new Object();
+//
+//     Constructor = [].shift.call(arguments);
+//
+//     obj.__proto__ = Constructor.prototype;
+//
+//     var ret = Constructor.apply(obj, arguments);
+//
+//     return typeof ret === 'object' ? ret : obj;
+//
+// };
+//
+// var person = objectFactory(Otaku, 'Kevin', '18')
+//
+// console.log(person.name) // Kevin
+// console.log(person.habit) // Games
+// console.log(person.strength) // 60
+//
+// person.sayYourName(); // I am Kevin
---
+++ ZTTUO/前端面试准备/new操作符.md
@@ -0,0 +1,30 @@
+## new实现过程
+
+- 创建一个空的简单JavaScript对象（即{}）；
+- 链接该对象（即设置该对象的构造函数）到另一个对象 ；
+- 将步骤1新创建的对象作为this的上下文 ；
+- 如果该函数没有返回对象，则返回this。
+
+## 手动实现
+
+```javascript
+function objectFactory() {
+
+    var obj = new Object(),//从Object.prototype上克隆一个对象
+
+    Constructor = [].shift.call(arguments);//取得外部传入的构造器
+
+    var F=function(){};
+    F.prototype= Constructor.prototype;
+    obj=new F();//指向正确的原型
+
+    var ret = Constructor.apply(obj, arguments);//借用外部传入的构造器给obj设置属性
+
+    return typeof ret === 'object' ? ret : obj;//确保构造器总是返回一个对象
+
+};
+```
+
+## 手动实现参考连接
+
+https://github.com/mqyqingfeng/Blog/issues/13
