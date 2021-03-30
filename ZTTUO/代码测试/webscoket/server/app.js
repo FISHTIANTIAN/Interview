1aab3594eb718f88fa7f5cb92ffaa41352d0dc21
tian.zhang <382785079@qq.com>
2021-03-30 23:44:34 +0800

    20210330[打卡]

--- ZTTUO/代码测试/webscoket/server/app.js
+++ ZTTUO/代码测试/webscoket/server/app.js
@@ -1,21 +1,37 @@
 const ws = require('nodejs-websocket');
 const PORT = 3000
+const TYPE_ON = 0
+const TYPE_OUT = 1
+const TYPE_MSG = 2
 
 var count = 0
 const server = ws.createServer(connect=>{
     console.log('有用户连接上来了')
     count++
     connect.userName = `用户${count}`
-    broadcast(`${connect.userName}进入了聊天室`)
+    broadcast({
+        type: TYPE_ON,
+        data: `${connect.userName}进入了聊天室`,
+        time: new Date().toLocaleTimeString()
+    })
     connect.on("text", (data)=>{
         console.log(data)
-        broadcast(data)
+        broadcast({
+            type: TYPE_MSG,
+            data: data,
+            time: new Date().toLocaleTimeString(),
+            name: `${connect.userName}`
+        })
     })
 
     connect.on("close", ()=>{
         console.log("连接关闭")
         count--
-        broadcast(`${connect.userName}离开了聊天室`)
+        broadcast({
+            type: TYPE_OUT,
+            data: `${connect.userName}离开了聊天室`,
+            time: new Date().toLocaleTimeString()
+        })
     })
 
     connect.on("error",()=>{
@@ -29,6 +45,6 @@ server.listen(PORT, ()=>{
 
 function broadcast(data) {
 	server.connections.forEach(function (conn) {
-		conn.sendText(data)
+		conn.sendText(JSON.stringify(data))
 	})
 }
