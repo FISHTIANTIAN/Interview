146569892a9797de8aa1d263582da3f776222492
tian.zhang <382785079@qq.com>
2021-07-23 14:49:05 +0800

    20210723[打卡]

--- ZTTUO/代码测试/测试代码/index.js
+++ ZTTUO/代码测试/测试代码/index.js
@@ -1,67 +1,57 @@
-console.log(1);
+// console.log(1);
 
-setTimeout(() => {
-    console.log(2);
-    setTimeout(() => {
-        console.log(14);
-        new Promise((resolve, reject) => {
-            console.log(15);
-            resolve()
-        }).then(res => {
-            console.log(16);
-        })
-    })
-    new Promise((resolve, reject) => {
-        console.log(3);
-        resolve()
-    }).then(res => {
-        console.log(4);
-    })
-})
+// setTimeout(() => {
+//     console.log(2);
+//     setTimeout(() => {
+//         console.log(14);
+//         new Promise((resolve, reject) => {
+//             console.log(15);
+//             resolve()
+//         }).then(res => {
+//             console.log(16);
+//         })
+//     })
+//     new Promise((resolve, reject) => {
+//         console.log(3);
+//         resolve()
+//     }).then(res => {
+//         console.log(4);
+//     })
+// })
 
-new Promise((resolve, reject) => {
-    resolve()
-}).then(res => {
-    console.log(5);
-}).then(res => {
-    console.log(6);
+// new Promise((resolve, reject) => {
+//     resolve()
+// }).then(res => {
+//     console.log(5);
+// }).then(res => {
+//     console.log(6);
+// })
 
-})
+// new Promise((resolve, reject) => {
+//     console.log(7);
+//     resolve()
+// }).then(res => {
+//     console.log(8);
+// }).then(res => {
+//     console.log(9);
+// })
 
-new Promise((resolve, reject) => {
-    console.log(7);
-    resolve()
-}).then(res => {
-    console.log(8);
-}).then(res => {
-    console.log(9);
-})
+// setTimeout(() => {
+//     console.log(10);
+//     new Promise((resolve, reject) => {
+//         console.log(11);
+//         resolve()
+//     }).then(res => {
+//         console.log(12);
+//     })
+// })
 
-setTimeout(() => {
-    console.log(10);
-    new Promise((resolve, reject) => {
-        console.log(11);
-        resolve()
-    }).then(res => {
-        console.log(12);
-    })
-})
+// console.log(13);
 
-console.log(13);
+// // 1 7 13 5 8 6 9 2 3 4 10 11 12 14 15 16
 
-VM1751:48 7
-VM1751:48 13
-VM1751:23 5
-VM1751:33 8
-VM1751:25 6
-VM1751:35 9
-undefined
-VM1751:2 2
-VM1751:13 3
-VM1751:16 4
-VM1751:39 10
-VM1751:41 11
-VM1751:44 12
-VM1751:4 14
-VM1751:6 15
-VM1751:9 16
+var b = 10; 
+(function a(){
+    b = 20
+})()
+console.log(b)
