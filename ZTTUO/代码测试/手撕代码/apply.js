74a9f054a09e53a388af27d72c8be8c0ffe4a3ec
tian.zhang <382785079@qq.com>
2021-08-04 17:57:11 +0800

    20210804[打卡]

--- ZTTUO/代码测试/手撕代码/apply.js
+++ ZTTUO/代码测试/手撕代码/apply.js
@@ -5,7 +5,7 @@ Function.prototype.ztApply = function (content, args) {
     }
     // 创造唯一的key值  作为我们构造的context内部方法名
     let fn = Symbol();
-    // this指向调用call的函数
+    // this指向调用apply的函数
     content[fn] = this;
     // 执行函数并返回结果 相当于把自身作为传入的context的方法进行调用了
     return content[fn](...args)
---
+++ ZTTUO/代码测试/手撕代码/bind.js
@@ -0,0 +1,32 @@
+Function.prototype.ztBind = function (content, ...args) {
+    // 用于判断第一个参数是否为空或者为null
+    if (!content || content === null) {
+        content = window
+    }
+    // 创造唯一的key值作为我们构造的context内部方法名
+    let fn = Symbol()
+    // this指向调用bind的函数
+    content[fn] = this;
+    let _this = this;
+    const result = function (...innerArgs) {
+        // 第一种情况: 若是将 bind 绑定之后的函数当作构造函数，通过 new 操作符使用，则不绑定传入的 this，而是将 this 指向实例化出来的对象
+        // 此时由于new操作符作用  this指向result实例对象  而result又继承自传入的_this 根据原型链知识可得出以下结论
+        // this.__proto__ === result.prototype; // this instanceof result =>true
+        // this.__proto__.__proto__ === result.prototype.__proto__ === _this.prototype; // this instanceof _this =>true
+        if (this instanceof _this === true) {
+            // 此时this指向指向result的实例  这时候不需要改变this指向
+            this[fn] = _this;
+            // 这里使用es6的方法让bind支持参数合并
+            this[fn](...[...args, ...innerArgs]);
+            delete this[fn];
+        } else {
+            // 如果只是作为普通函数调用  那就很简单了 直接改变this指向为传入的context
+            content[fn](...[...args, ...innerArgs]);
+            delete this[fn];
+        }
+    }
+    // 如果绑定的是构造函数 那么需要继承构造函数原型属性和方法
+    // 实现继承的方式: 使用Object.create
+    result.prototype = Object.create(this.prototype);
+    return result;
+}
