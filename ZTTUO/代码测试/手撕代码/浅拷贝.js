cd447d34a93d8ff3589beb4c29efd1f634f0ac37
tian.zhang <382785079@qq.com>
2021-08-30 11:55:18 +0800

    20210830[打卡]

---
+++ ZTTUO/代码测试/手撕代码/浅拷贝.js
@@ -0,0 +1,14 @@
+// 20210827
+const shallowClone = (target) => {
+    if (typeof target === 'object' && target !== null) {
+        const cloneTarget = Array.isArray(target) ? [] : {};
+        for (let prop in target) {
+            if (target.hasOwnProperty(prop)) {
+                cloneTarget[prop] = target[prop];
+            }
+        }
+        return cloneTarget;
+    } else {
+        return target;
+    }
+}
