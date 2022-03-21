f0fe312c387562194f6ae87600b6c628c43961a4
zhangtian <382785079@qq.com>
2022-03-21 17:49:50 +0800

    【20220321打卡】

---
+++ ZTTUO/代码测试/手撕代码/url截取.js
@@ -0,0 +1,6 @@
+const getUrlParam = (key) => {
+    let reg = new RegExp('(^|&)' + key + '=([^&]*)(&|$)')
+    let r = window.location.search.substr(1).match(reg)
+    if (r != null) return unescape(r[2])
+    return null
+}
---
+++ ZTTUO/代码测试/算法/数组/350. 两个数组的交集 II.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+    
+};
+
+console.log(intersect([1, 2, 2, 3], [2, 3]))
